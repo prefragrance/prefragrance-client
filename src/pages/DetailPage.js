e89d4755ab6665576a4d18fafe49854294eca5e5
jellysoo97 <jellysoo97@gmail.com>
2022-08-31 23:43:51 +0900

    Refactor: DetailPage내 index.js 추가

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Content from '../components/DetailPage/Content';
-import Tab from '../components/DetailPage/Tab';
-import ProductDetail from '../components/DetailPage/ProductDetail';
+import { Content, ProductDetail, Tab } from '../components/DetailPage';
 
 const DetailPage = () => {
   return (
