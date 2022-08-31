fbc1f02d05c01e3159209915620cb0acd977d123
jellysoo97 <jellysoo97@gmail.com>
2022-08-31 23:24:57 +0900

    Refactor: App.js 수정, Template.js 삭제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import GlobalStyle from './GlobalStyle';
-import Template from './Template';
-import MainContainer from './components/shared/MainContainer';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Nav from './components/shared/Nav';
-import Search from './pages/Search';
 import Footer from './components/shared/Footer';
 
-const App = () => {
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+function App() {
   return (
     <>
       <GlobalStyle />
-      <Template>
-        <Nav />
-        <MainContainer>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/detail" element={<Detail />} />
-            <Route path="/search" element={<Search />} />
-          </Routes>
-        </MainContainer>
-        <Footer />
-      </Template>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+        </Route>
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
