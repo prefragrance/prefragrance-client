9aba11be8180819ce8d3d311faeaa82ec3fac62c
jellysoo97 <jellysoo97@gmail.com>
2022-09-10 01:17:38 +0900

    fix: react-hook-form 없이 form 생성

--- src/components/shared/SearchBar/Modal/index.jsx
+++ src/components/shared/SearchBar/Modal/index.jsx
@@ -15,15 +15,15 @@ const Modal = () => {
 
 const ModalSection = styled.div`
   position: absolute;
-  top: 3.2rem;
-  width: calc(100% - 95px - 7rem); // 나중에 SearchBar width랑 동기화시켜야 함
+  top: 52px;
+  width: 600px; // 나중에 SearchBar width랑 동기화시켜야 함
   min-height: 100px;
   background-color: ${COLOR.WHITE};
   z-index: 99;
-  box-size: border-box;
+  box-sizing: border-box;
   border: 1px solid ${COLOR['GREEN-100']};
   padding: 0.4rem;
-  border-radius: 0 0 0.4rem 0.4rem;
+  border-radius: 0.4rem;
 `;
 
 export default Modal;
