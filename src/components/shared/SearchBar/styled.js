02d46657f8dd385bc1f15202ff0f8b019c998635
zizonyoungjun <naya700@naver.com>
2022-09-10 11:16:41 +0900

    Fix: searchBar radius 0.8

--- src/components/shared/SearchBar/styled.js
+++ src/components/shared/SearchBar/styled.js
@@ -11,14 +11,14 @@ export const SearchForm = styled.form`
     padding: 1rem;
     padding-left: 7.5rem;
     padding-right: 9rem;
-    border-radius: 2rem;
+    border-radius: 0.8rem;
     width: 100%;
   }
   button {
     font-size: 1.25rem;
     width: 7rem;
     height: 3.2rem;
-    border-radius: 2rem;
+    border-radius: 0.8rem;
     position: absolute;
     right: 0;
   }
@@ -29,13 +29,13 @@ export const Select = styled.select`
   position: absolute;
   width: 6.5rem;
   height: 3.2rem;
-  border-top-left-radius: 2rem;
-  border-bottom-left-radius: 2rem;
+  border-top-left-radius: 0.8rem;
+  border-bottom-left-radius: 0.8rem;
   left: 0rem;
   top: 0;
   padding: 0.8rem;
-  padding-left: 1.6rem;
-  background: ${COLOR['GREEN-200']};
-  font-size: 1.1rem;
+  padding-left: 1.1rem;
+  background: ${COLOR['GREEN-100']};
+  font-size: 1rem;
   color: BLACK;
 `;
