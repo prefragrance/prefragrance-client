d358be0f044dcc30e63acd2023752a8e45708659
HiImConan <mindol0320@naver.com>
2022-09-19 11:02:34 +0900

    Feat: 최근검색어 삭제버튼 추가

--- src/components/shared/SearchBar/Modal/SearchModal/KeywordList/KeywordBlock.jsx
+++ src/components/shared/SearchBar/Modal/SearchModal/KeywordList/KeywordBlock.jsx
@@ -2,25 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import COLOR from '../../../../../../constants/color';
 import { IoSearchCircle } from 'react-icons/io5';
+import { HiOutlineX } from 'react-icons/hi';
+import { deleteRecentSearchEach } from '../../../../../../hook/useLocal';
 
 const KeywordBlock = ({ text }) => {
+  const onDeleteRecentKeyword = () => {
+    deleteRecentSearchEach(text);
+  };
+
   return (
     <Wrapper>
-      <IoSearchCircle color={COLOR['GRAY-200']} fontSize="1.5rem" />
-      {text}
+      <QuerySection>
+        <IoSearchCircle color={COLOR['GRAY-200']} fontSize="1.5rem" />
+        {text}
+      </QuerySection>
+      <DeleteBtnSection>
+        <HiOutlineX
+          color={COLOR['GRAY-200']}
+          fontSize="0.9rem"
+          opacity="0.5"
+          onClick={onDeleteRecentKeyword}
+        />
+      </DeleteBtnSection>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.li`
   display: flex;
+  position: relative;
+  justify-content: space-between;
   align-items: center;
-  gap: 0.5rem;
   padding: 13px 15px;
   // border-bottom: 1px solid ${COLOR['GRAY-100']}; 없는게 더 나은 것 같기도?
   &:hover {
     background-color: ${COLOR['GRAY-100']};
   }
+`;
+
+const QuerySection = styled.div`
+  gap: 0.5rem;
+  display: flex;
+  align-items: center;
+`;
+const DeleteBtnSection = styled.div`
+  cursor: pointer;
 `;
 
 export default KeywordBlock;
