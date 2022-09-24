fd844a193537576ae24b8442fb082ddb6498b9fe
jellysoo97 <jellysoo97@gmail.com>
2022-09-24 22:13:35 +0900

    Fix: api 주소 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,17 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/search',
-    createProxyMiddleware({
-      target: 'http://localhost:8000',
-      changeOrigin: true,
-    }),
-  );
-};
-
-module.exports = function (app) {
-  app.use(
-    '/accounts',
+    '/api',
     createProxyMiddleware({
       target: 'http://localhost:8000',
       changeOrigin: true,
