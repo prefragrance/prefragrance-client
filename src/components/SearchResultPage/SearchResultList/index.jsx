05d257d0ebc5330795d12acf505fad716e5a1923
HiImConan <mindol0320@naver.com>
2022-09-25 21:22:12 +0900

    Feat: 검색 결과 페이지 UI 수정(1차)

--- src/components/SearchResultPage/SearchResultList/index.jsx
+++ src/components/SearchResultPage/SearchResultList/index.jsx
@@ -17,9 +17,11 @@ const SearchResultList = ({ searchResultData }) => {
 const Container = styled.div`
   width: 1200px;
   height: auto;
+  min-height: 100%; // 세로 최솟값 더 정확하게 연동시키는 방법?
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
+  background-color: ${COLOR.gray[100]};
 `;
 
 export default SearchResultList;
