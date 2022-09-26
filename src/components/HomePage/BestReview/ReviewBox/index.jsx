8385475913fd5d2c2d024e945b2fd4bf9799dc17
HiImConan <mindol0320@naver.com>
2022-09-26 23:23:07 +0900

    Style: HomePage 컴포넌트들 코드 스타일 최신화

--- src/components/HomePage/BestReview/ReviewBox/index.jsx
+++ src/components/HomePage/BestReview/ReviewBox/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  Img,
-  ReviewSection,
-  Content,
-  InfoSection,
-  ReviewInfo,
-  Title,
-} from './styled';
+import styled from 'styled-components';
+import COLOR from 'constants/color';
+import DefaultImg from 'assets/images/취향로고.png';
 
 const ReviewBox = ({ url, content, nickname, date, producer, name }) => {
   return (
@@ -25,6 +20,47 @@ const ReviewBox = ({ url, content, nickname, date, producer, name }) => {
       </InfoSection>
     </ReviewSection>
   );
+};
+
+const ReviewSection = styled.div`
+  display: grid;
+  grid-template-rows: 3fr 2fr;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+  width: 80%;
+  background: ${COLOR.white};
+`;
+
+const Img = styled.img`
+  border: 1px solid ${COLOR.black};
+`;
+
+const InfoSection = styled.div`
+  display: grid;
+  grid-template-rows: 2fr 1fr 1.5fr;
+`;
+
+const Content = styled.div`
+  word-wrap: break-word;
+  text-overflow: ellipsis;
+`;
+
+const ReviewInfo = styled.div`
+  font-size: 0.5rem;
+  color: #ebebeb;
+  & > span::after {
+    content: '|';
+  }
+  & > span:first-child::after {
+    content: '';
+  }
+`;
+
+const Title = styled.div``;
+
+Img.defaultProps = {
+  src: DefaultImg,
 };
 
 export default ReviewBox;
